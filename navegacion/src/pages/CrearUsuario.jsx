89577b19651a47af839238ff4f01893ae0cb6935
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2023-10-25 10:51:44 -0500

    react

--- navegacion/src/pages/CrearUsuario.jsx
+++ navegacion/src/pages/CrearUsuario.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React,{useEffect,useRef} from "react";
+import api from '../components/api'
+import {useNavigate} from 'react-router-dom'
 
 
-const Crear = () =>{
+const crearUsuario = () =>{
+
+  const name = useRef();
+  const email = useRef();
+  const password = useRef();
+
+  const navigate = useNavigate();
+  
+  useEffect(()=>{
+    name.current.focus();
+  },[])
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const data = {
+      name:name.current.value,
+      email: email.current.value,
+      password: password.current.value
+    };
+    api.post('/user',data,{}).finally(()=>{
+      navigate('/usuarios');
+    })
+  }
   return (
-    <div>
-      <h1>Esta es la pagina de Crear</h1>
-    </div>
+    <form onSubmit={handleSubmit} method="post">
+      <h1 className="text-center font-bold underline text-3-xl p-3 m-3">Crear Usuario</h1>
+      <div className="max-w-xs">
+        <input type="text" className="shadow appearance-none border rounded w-full p-2 m-2 text-gray-700 laeding-tight focus:outline-none focus:shodow-outline" id="name" name="name" ref={name} placeholder="Ingrese nombre"/>
+      </div>
+      <div className="max-w-xs">
+        <input type="email" className="shadow appearance-none border rounded w-full p-2 m-2 text-gray-700 laeding-tight focus:outline-none focus:shodow-outline" id="email" name="email" ref={email} placeholder="Ingrese email"/>
+      </div>
+      <div className="max-w-xs">
+        <input type="password" className="shadow appearance-none border rounded w-full p-2 m-2 text-gray-700 laeding-tight focus:outline-none focus:shodow-outline" id="password" name="password" ref={password} placeholder="Ingrese Contaseña"/>
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded focus:outline-none focus:shadow-outline" type="submit">Registrar Usuario</button>
+    </form>
   );
 }
 
-export default Crear;
+export default crearUsuario;
--- navegacion/src/pages/EditarUsuario.jsx
+++ navegacion/src/pages/EditarUsuario.jsx
@@ -1,12 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../components/api";
 
+const EditarUsuario = () => {
+  const { id } = useParams();
+  const [usuario, setUsuario] = useState({ name: "", email: "" });
+  const navigate = useNavigate();
 
-const Editar = () =>{
+  useEffect(() => {
+    const buscarUsuario = async () => {
+      try {
+        const res = await api.get(`/user/${id}`);
+        setUsuario(res.data);
+      } catch (e) {
+        console.log("Error buscando el usuario", e);
+      }
+    };
+    buscarUsuario();
+  }, [id]);
+
+  const handleEditUser = async () => {
+    try {
+      await api.put(`/user/${id}`,usuario);
+      navigate("/usuarios")
+    } catch (e) {
+      console.log("Error en handleEditUser", e);
+    }
+  };
+  const handleDeleteUser = async () => {
+    try {
+      await api.delete(`/user/${id}`,usuario);
+      navigate("/usuarios")
+    } catch (e) {
+      console.log("Error en handleDeleteUser", e);
+    }
+  };
+
   return (
     <div>
-      <h1>Esta es la pagina de Editar</h1>
+      <h1 className="text-center font-bold underline text-3-xl p-3 m-3">
+        Editar Usuario
+      </h1>
+      <div className="max-w-xs">
+        <input
+          type="text"
+          className="shadow appearance-none border rounded w-full p-2 m-2 text-gray-700 laeding-tight focus:outline-none focus:shodow-outline"
+          id="name"
+          name="name"
+          placeholder="Ingrese nombre"
+          value={usuario.name}
+          onChange={(e) => {
+            setUsuario({ ...usuario, name: e.target.value });
+          }}
+        />
+      </div>
+      <div className="max-w-xs">
+        <input
+          type="email"
+          className="shadow appearance-none border rounded w-full p-2 m-2 text-gray-700 laeding-tight focus:outline-none focus:shodow-outline"
+          id="email"
+          name="email"
+          placeholder="Ingrese email"
+          value={usuario.email}
+          onChange={(e) => {
+            setUsuario({ ...usuario, email: e.target.value });
+          }}
+        />
+      </div>
+      <div className="max-w-xs">
+        <input
+          type="password"
+          className="shadow appearance-none border rounded w-full p-2 m-2 text-gray-700 laeding-tight focus:outline-none focus:shodow-outline"
+          id="password"
+          name="password"
+          placeholder="Ingrese Contaseña"
+          value={usuario.password}
+          onChange={(e) => {
+            setUsuario({ ...usuario, password: e.target.value });
+          }}
+        />
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded focus:outline-none focus:shadow-outline"
+        type="submit"
+        onClick={handleEditUser}
+      >
+        Editar Usuario
+      </button>
+      <button
+        className="bg-rose-500 hover:bg-rose-700 text-white font-bold p-2 m-2 rounded focus:outline-none focus:shadow-outline"
+        type="submit"
+        onClick={handleDeleteUser}
+      >
+        Eliminar Usuario
+      </button>
     </div>
   );
-}
+};
 
-export default Editar;
+export default EditarUsuario;
